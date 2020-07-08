29bb3d7eda6d5d421cf30b083319f9e4288dbdf7
Phan Tam <tampv.bg@gmail.com>
2020-07-08 23:36:04 +0700

    fix log

--- wp-content/themes/idc_blog/sidebar.php
+++ wp-content/themes/idc_blog/sidebar.php
@@ -36,7 +36,7 @@ $recent_posts = wp_get_recent_posts(array(
 <div class="col-lg-4">
    <div class="blog_right_sidebar">
       <aside class="single_sidebar_widget search_widget">
-         <form action="<?php echo get_search_link(); ?>">
+         <form action="/">
             <div class="form-group">
                <div class="input-group mb-3">
                   <input type="text" class="form-control" name="s" value="<?php echo get_search_query(); ?>" placeholder="Từ khóa tìm kiếm">
