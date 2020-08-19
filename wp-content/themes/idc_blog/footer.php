a304e20387bc1d304c650809bbf75abf1f75ed9e
Phan Tam <tampv.bg@gmail.com>
2020-08-19 17:22:32 +0700

    fix: js

--- wp-content/themes/idc_blog/footer.php
+++ wp-content/themes/idc_blog/footer.php
@@ -107,14 +107,14 @@
     }
 </script>
 <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
-    async defer>
+    async defer />
   <!--Start of Tawk.to Script-->
   <script type="text/javascript">
     var Tawk_API=Tawk_API||{},Tawk_LoadStart=new Date();(function(){var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];s1.async=true;s1.src='https://embed.tawk.to/5423d8fad788df513200062b/default';s1.charset='UTF-8';s1.setAttribute('crossorigin','*');s0.parentNode.insertBefore(s1,s0);})();
   </script>
   <!--End of Tawk.to Script-->
 </body>
-</html>k.to/5423d8fad788df513200062b/default';s1.charset='UTF-8';s1.setAttribute('crossorigin','*');s0.parentNode.insertBefore(s1,s0);})();
+</html>
   </script>
   <!--End of Tawk.to Script-->
 </body>
